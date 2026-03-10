60178bb9a7a22ac0d48aef3f6731ced4e208ae53
Your Name <you@example.com>
2026-03-10 16:14:53 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,115 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(
     MaterialApp(
-     debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.deepOrangeAccent,
-          title: Text("Log in page",style: TextStyle(color: Colors.white),),
-         centerTitle: true,
+        backgroundColor: Colors.white,
+        // appBar: AppBar(
+        //   backgroundColor: Colors.pinkAccent,
+        //   title: Text('LogIn Page', style: TextStyle(color: Colors.white)),
+        //   centerTitle: true,
+        // ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Text(
+              //   "Jumia Market Place",
+              //   style: TextStyle(
+              //     color: Colors.purple,
+              //     fontSize: 18.5,
+              //     fontWeight: FontWeight.w800,
+              //   ),
+              // ),
+              Image.asset('assets/jumia.png'),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Enter Username',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hint: Text("Email or Phone Number"),
 
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          //crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-    
-        Text(
-          "My Application",
-        style: TextStyle(
-          color:Colors.blue,
-          fontSize: 20,
-          fontWeight: FontWeight.w800),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Enter Password',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hint: Text("PIN or Password"),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    prefixIcon: Icon(Icons.lock),
+                  ),
+                ),
+              ),
+              // MaterialButton(
+              //   onPressed: () {},
+              //   child: Text('LogIn'),
+              //   color: Colors.pinkAccent,
+              //   textColor: Colors.white,
+              // ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+
+                  child: Text(
+                    "Log In",
+                    style: TextStyle(color: Colors.white, fontSize: 14),
+                  ),
+                ),
+              ),
+            ],
           ),
-          Text("Enter Password"),
-          TextField(),
-          Text("Enter Username"),
-          TextField(),
-          SizedBox(height: 20,),
-          MaterialButton(onPressed:(){},child: Text("Login", style: TextStyle(color: Colors.white,)),color: Colors.deepOrangeAccent,)
-          ],
-        )
-      )
-    )
+        ),
+      ),
+    ),
   );
 }
