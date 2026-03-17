d938fc8fde2072d04a8a0fa9e6790e794e57fc82
Your Name <you@example.com>
2026-03-17 20:38:40 +0300

    Sign up page

--- lib/main.dart
+++ lib/main.dart
@@ -6,107 +6,195 @@ void main() {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        // appBar: AppBar(
-        //   backgroundColor: Colors.pinkAccent,
-        //   title: Text('LogIn Page', style: TextStyle(color: Colors.white)),
-        //   centerTitle: true,
-        // ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // Text(
-              //   "Jumia Market Place",
-              //   style: TextStyle(
-              //     color: Colors.purple,
-              //     fontSize: 18.5,
-              //     fontWeight: FontWeight.w800,
-              //   ),
-              // ),
-              Image.asset('assets/jumia.png'),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Enter Username',
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w800,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hint: Text("Email or Phone Number"),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 60),
+                Image.asset('assets/jumia.png'),
+                SizedBox(height: 10),
 
-                    border: OutlineInputBorder(
+                // Full Name
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Enter Full Name',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Full Name",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      prefixIcon: Icon(Icons.person),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                // Email
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Enter Email',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Email Address",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      prefixIcon: Icon(Icons.email),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                // Phone Number
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Enter Phone Number',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Phone Number",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      prefixIcon: Icon(Icons.phone),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                // Password
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Enter Password',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "PIN or Password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      prefixIcon: Icon(Icons.lock),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                // Confirm Password
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Confirm Password',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Confirm PIN or Password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      prefixIcon: Icon(Icons.lock_outline),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                // Sign Up Button
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: Container(
+                    height: 50,
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
                       borderRadius: BorderRadius.circular(20),
                     ),
-                    prefixIcon: Icon(Icons.person),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(25, 0, 20, 5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Enter Password',
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w800,
-                      ),
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(color: Colors.white, fontSize: 14),
                     ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hint: Text("PIN or Password"),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    prefixIcon: Icon(Icons.lock),
                   ),
                 ),
-              ),
-              // MaterialButton(
-              //   onPressed: () {},
-              //   child: Text('LogIn'),
-              //   color: Colors.pinkAccent,
-              //   textColor: Colors.white,
-              // ),
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                child: Container(
-                  height: 50,
-                  width: double.infinity,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
 
-                  child: Text(
-                    "Log In",
-                    style: TextStyle(color: Colors.white, fontSize: 14),
-                  ),
-                ),
-              ),
-            ],
+                SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
